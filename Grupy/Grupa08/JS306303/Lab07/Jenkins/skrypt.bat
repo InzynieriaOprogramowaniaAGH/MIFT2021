32fbc09aac7c1b715286b90bf14a1ae471623d4e
xTensu <kubva_s126@op.pl>
2021-05-05 16:38:48 +0200

    zad2

--- Grupy/Grupa08/JS306303/Lab07/Jenkins/skrypt.bat
+++ Grupy/Grupa08/JS306303/Lab07/Jenkins/skrypt.bat
@@ -7,6 +7,7 @@ docker run --name jenkins-docker --rm --detach ^
   
 docker run --name jenkins-blueocean --rm --detach ^
   --network jenkins --env DOCKER_HOST=tcp://docker:2376 ^
+  --user root ^
   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 ^
   --volume jenkins-data:/var/jenkins_home ^
   --volume jenkins-docker-certs:/certs/client:ro ^
