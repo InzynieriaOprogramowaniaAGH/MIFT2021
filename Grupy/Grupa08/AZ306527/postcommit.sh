fcf100821e40aa84a2db79565c4fbbcb58b6110d
zarczynsky <54021935+zarczynsky@users.noreply.github.com>
2021-05-13 16:24:26 +0200

    Update postcommit.sh

--- Grupy/Grupa08/AZ306527/postcommit.sh
+++ Grupy/Grupa08/AZ306527/postcommit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Uruchamiamy Pipeline"
-curl -X POST -L --user admin:11b18c0eaee3e6da9fccf6dd070101bade http://localhost:8080/job/wire-desktop-3/build
+curl -X POST -L --user admin:key http://localhost:8080/job/wire-desktop-3/build
